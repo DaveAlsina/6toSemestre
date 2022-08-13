8872f741eae3cdaa556253d15c3801fc0a63b107
DaveAlsina <davealsina@gmail.com>
2022-08-13 18:00:12 -0500

    Sync of files from: rociante Sat Aug 13 06:00:12 PM -05 2022

--- ia/miniproyectos/mini1/tiempos.py
+++ ia/miniproyectos/mini1/tiempos.py
@@ -1,5 +1,5 @@
 from time import time
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -27,31 +27,27 @@ def compara_funciones(funs = None, arg = None, nombres = None, N=10):
 def compara_funciones_sobreargumentos(funs, args, nombres, N=10):
     data_list = []
 
-    #progress bar that shows the progress of the function, 
-    #in only one line with  \r to overwrite the previous line
+    pbar = tqdm(total=len(funs), desc='\rComparando funciones', position=0, leave=True)
+
+    for i, arg in enumerate(args):
+        nms = []
+        argumentos = []
+        ts = []
+
+        pbar2 = tqdm(total=len(funs), desc='\rProbando función seleccionada iter. {}'.format(i),
+                     position=1,
+                     leave=True)
+
         for i, fun in enumerate(funs):
-            data = compara_funciones(fun, args[i], nombres[i], N)
-            data_list.append(data)
+            nms += [nombres[i] for x in range(N)]
+            argumentos += [i for x in range(N)]
+            ts += obtiene_tiempos(fun, [arg], N)
 
+            pbar2.update()
 
-
-    with tqdm(total=len(funs), desc='\rComparando funciones',) as pbar:
-
-        for i, arg in enumerate(args):
-            nms = []
-            argumentos = []
-            ts = []
-
-        with tqdm(total=len(funs), desc='\rComparando funciones',) as pbar2:
-
-            for i, fun in tqdm(enumerate(funs), desc='\rProbando en funciones escogidas', total=len(funs)):
-                nms += [nombres[i] for x in range(N)]
-                argumentos += [i for x in range(N)]
-                ts += obtiene_tiempos(fun, [arg], N)
-                pbar2.update(1)
 
         data_list.append(pd.DataFrame({'Función':nms, 'Num_argumento':argumentos, 'Tiempo':ts}))
-        pbar.update(1)
+        pbar.update()
 
     return pd.concat(data_list)
 
