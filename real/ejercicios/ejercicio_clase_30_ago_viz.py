515de2463fe03f3e1202defadd00d420c92e9cea
DaveAlsina <davealsina@gmail.com>
2022-09-04 18:00:12 -0500

    Sync of files from: rociante Sun Sep  4 06:00:12 PM -05 2022

--- real/ejercicios/ejercicio_clase_30_ago_viz.py
+++ real/ejercicios/ejercicio_clase_30_ago_viz.py
@@ -1,17 +1,21 @@
 import numpy as np 
 from matplotlib import pyplot as plt
 
+def e(c: float, exponent: float) -> float:
+    return np.power(c, exponent)
 
 def f(c: float, npoints: int) -> None:
 
-    in_points = np.ones((1, npoints))*c 
-    exponents = 1/np.arange(1, in_points.shape[0])
+    in_points = np.ones((npoints, ))*c 
+    exponents = 1/np.arange(1, in_points.shape[0]+1)
 
-    vfun = np.vectorize()
+    print(in_points.shape, exponents.shape)
+
+    vfun = np.vectorize(pyfunc = e,  otypes=[float])
     out  = vfun(in_points, exponents)
 
-    plt.plot()
+    plt.plot(out)
+    plt.show()
 
-
-
+f(c = 20, npoints = 1000)
 
